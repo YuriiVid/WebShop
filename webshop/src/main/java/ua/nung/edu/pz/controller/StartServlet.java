154d677cd1165c04ef99c475f33651e0fd762605
YuriiVid <yuriividoniak@gmail.com>
2024-02-28 16:46:47 +0200

    add User model, add IndexView view

--- webshop/src/main/java/ua/nung/edu/pz/controller/StartServlet.java
+++ webshop/src/main/java/ua/nung/edu/pz/controller/StartServlet.java
@@ -8,6 +8,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import ua.nung.edu.pz.view.IndexView;
 
 @WebServlet(name = "StartServlet", urlPatterns = "/*", loadOnStartup = 1)
 public class StartServlet extends HttpServlet {
@@ -16,12 +17,7 @@ public class StartServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = resp.getWriter();
-		out.println("<html>\r\n" + 
-						"<body>\r\n" + 
-						"<h2>Hello World From Servlet!</h2>\r\n" + 
-						"</body>\r\n" + 
-						"</html>\r\n" + 
-						"");
+		out.println(IndexView.getPage("Welcome to our shop","<h2>Hello World From Servlet!</h2>"));
 	}
 
 }
