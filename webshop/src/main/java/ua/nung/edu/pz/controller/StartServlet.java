d95082bac3250858149786b7fe8dcb4496c62997
YuriiVid <yuriividoniak@gmail.com>
2024-03-03 20:48:34 +0200

    add header, footerm login form and ContactView

--- webshop/src/main/java/ua/nung/edu/pz/controller/StartServlet.java
+++ webshop/src/main/java/ua/nung/edu/pz/controller/StartServlet.java
@@ -1,23 +1,49 @@
 package ua.nung.edu.pz.controller;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import ua.nung.edu.pz.model.User;
+import ua.nung.edu.pz.view.ContactView;
 import ua.nung.edu.pz.view.IndexView;
 
-@WebServlet(name = "StartServlet", urlPatterns = "/*", loadOnStartup = 1)
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "StartServlet", urlPatterns = { "/*" }, loadOnStartup = 1)
 public class StartServlet extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("text/html;charset=UTF-8");
-		PrintWriter out = resp.getWriter();
-		out.println(IndexView.getPage("Welcome to our shop","<h2>Hello World From Servlet!</h2>"));
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+
+		String context = "<h2>Hello World from Servlet!</h2>\n";
+		context += IndexView.getLoginForm();
+
+		String body = IndexView.getBody(
+				IndexView.getHeader(""),
+				IndexView.getFooter(""),
+				context);
+		body += ContactView.getContactForm();
+		out.println(IndexView.getPage("Welcome to the Shop", body));
 	}
 
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// String contextPath = request.getContextPath();
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		User user = new User();
+		user.setEmail(email);
+		user.setPassword(password);
+
+		System.out.println(user);
+
+		response.sendRedirect("/");
+	}
 }
--- webshop/src/main/java/ua/nung/edu/pz/view/IndexView.java
+++ webshop/src/main/java/ua/nung/edu/pz/view/IndexView.java
@@ -18,4 +18,73 @@ public class IndexView {
 				"  </body>\r\n" + //
 				"</html>";
 	}
+
+	public static String getBody(String header, String footer, String context) {
+		return header +
+				"<div class=\"container\">" +
+				context +
+				"</div>" +
+				footer;
+	}
+
+	public static String getHeader(String header) {
+		return "<nav class=\"navbar navbar-expand-lg bg-body-tertiary\">\n" +
+				"  <div class=\"container-fluid\">\n" +
+				"    <a class=\"navbar-brand\" href=\"/\">GreenShop</a>\n" +
+				"    <button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarNav\" aria-controls=\"navbarNav\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n"
+				+
+				"      <span class=\"navbar-toggler-icon\"></span>\n" +
+				"    </button>\n" +
+				"    <div class=\"collapse navbar-collapse\" id=\"navbarNav\">\n" +
+				"      <ul class=\"navbar-nav\">\n" +
+				"        <li class=\"nav-item\">\n" +
+				"          <a class=\"nav-link active\" aria-current=\"page\" href=\"/\">Home</a>\n" +
+				"        </li>\n" +
+				"        <li class=\"nav-item\">\n" +
+				"          <a class=\"nav-link\" href=\"/pricing\">Pricing</a>\n" +
+				"        </li>\n" +
+				"\t<li class=\"nav-item\">\n" +
+				"          <a class=\"nav-link\" href=\"/contacts\">Contacts</a>\n" +
+				"        </li>\n" +
+				"      </ul>\n" +
+				"    </div>\n" +
+				"  </div>\n" +
+				"</nav>";
+	}
+
+	public static String getFooter(String footer) {
+		return "<footer class=\"container-fluid mt-5\">\n" +
+				"<div class=\"bg-dark text-secondary px-4 py-5 text-center\">\n" +
+				"    <div class=\"py-5\">\n" +
+				"      <h1 class=\"display-5 fw-bold text-white\"> <span class=\"text-success\">Green Shop</span>  </h1>\n"
+				+
+				"      <div class=\"col-lg-6 mx-auto\">\n" +
+				"        <p class=\"fs-5 mb-4\">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>\n"
+				+
+				"        <div class=\"d-grid gap-2 d-sm-flex justify-content-sm-center\">\n" +
+				"          <button type=\"button\" class=\"btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold\">Custom button</button>\n"
+				+
+				"          <button type=\"button\" class=\"btn btn-outline-light btn-lg px-4\">Secondary</button>\n" +
+				"        </div>\n" +
+				"      </div>\n" +
+				"    </div>\n" +
+				"  </div>\n" +
+				"</footer>";
+	}
+
+	public static String getLoginForm() {
+		return "<form action=\"login\" class=\"my-5\" method=\"post\">\n" +
+				"  <div class=\"mb-3\">\n" +
+				"    <label for=\"webshop-login-form-username\" class=\"form-label\">Email address</label>\n" +
+				"    <input type=\"text\" class=\"form-control\" id=\"webshop-login-form-username\" name=\"username\">\n"
+				+
+				"  </div>\n" +
+				"  <div class=\"mb-3\">\n" +
+				"    <label for=\"webshop-login-form-password\" class=\"form-label\">Password</label>\n" +
+				"    <input type=\"password\" class=\"form-control\" id=\"webshop-login-form-password\" name=\"password\">\n"
+				+
+				"  </div>\n" +
+				"  <button type=\"submit\" class=\"btn btn-primary\">Submit</button>\n" +
+				"</form>";
+	}
 }
