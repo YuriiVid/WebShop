e16a9142470954ea37a229d5619f4e2255f91e52
YuriiVid <yuriividoniak@gmail.com>
2024-04-15 09:51:45 +0300

    add photo integration

--- webshop/src/main/java/ua/nung/edu/pz/controller/GoodsServlet.java
+++ webshop/src/main/java/ua/nung/edu/pz/controller/GoodsServlet.java
@@ -26,16 +26,18 @@ public class GoodsServlet extends HttpServlet {
 		// testing data start
 
 		GoodRepository goodRepository = new GoodRepository();
-		ArrayList<Good> goods = goodRepository.getAllGoods();
+		ArrayList<Good> goods = goodRepository.getByBrand("Naturalis");
 
 		String body = goods.stream().map(good -> {
 			return "<div class=\"col-12 col-sm-6 col-lg-4 col-xl-3 my-2\">" +
 					"<div class=\"card\" style=\"width: 18rem;\">\n" +
+					"<img src=\"/img/" + (good.getPhoto().length > 0 ? good.getPhoto()[0] : "")
+					+ "\" class=\"card-img-top\" alt=\"good image\">" +
 					"  <div class=\"card-body\">\n" +
 					"    <h5 class=\"card-title\">" + good.getName() + "</h5>\n" +
-					"    <h6 class=\"card-subtitle mb-2 text-body-secondary\">" + "Ціна: "
-					+ good.getPrice().getFor_client() + "грн</h6>\n" +
-					"    <p class=\"card-text\">" + good.getDescription() + "</p>\n" +
+					"    <h6 class=\"card-subtitle mb-2 text-body-secondary\">Price:" + good.getPrice().getFor_client()
+					+ " UAH</h6>\n" +
+					"    <p class=\"card-text\">" + good.getShortDescription() + "</p>\n" +
 					"    <a href=\"#\" class=\"card-link\">Card link</a>\n" +
 					"    <a href=\"#\" class=\"card-link\">Another link</a>\n" +
 					"  </div>\n" +
