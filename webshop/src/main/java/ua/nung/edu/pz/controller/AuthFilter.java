d413976c63626618416fd4851b8f0f775afd6e2c
YuriiVid <yuriividoniak@gmail.com>
2024-04-29 09:25:31 +0300

    add authFilter

---
+++ webshop/src/main/java/ua/nung/edu/pz/controller/AuthFilter.java
@@ -0,0 +1,42 @@
+package ua.nung.edu.pz.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebFilter(filterName = "AuthFilter", urlPatterns = "/cart/*")
+public class AuthFilter implements Filter {
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		Filter.super.init(filterConfig);
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		HttpSession session = req.getSession(false);
+		if (session != null && session.getAttribute("user") != null) {
+			chain.doFilter(request, response);
+		} else {
+			resp.sendRedirect("/");
+		}
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		Filter.super.destroy();
+	}
+}
